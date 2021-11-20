26c52cf70227b7b0d251d9322bcb33e4be3cc184
Art <artemgerasimov55@gmail.com>
2021-11-20 15:14:50 +0200

    update Lab6.c

--- Year 1/Computer Science/Code/Lab6.c
+++ Year 1/Computer Science/Code/Lab6.c
@@ -19,72 +19,100 @@ int Sorted(int);
 //---------------------
 // EX 4 FUNCTIONS
 void Ex4();
-
+int Checknumbers(int);
 //---------------------
 // EX 5 FUNCTION
 void Ex5();
-
-int main()
-{
+void DigitsCounter(int);
+void ReversedDigitsCounter(int);
+int DigitsRange(int);
+//---------------------
+int main() {
     //Ex1();
     //Ex2();
-    Ex3();
+    //Ex3();
     //Ex4();
-    //Ex5();
+    Ex5();
     return 0;
 }
 
-void Ex1()
-{
+void Ex1() {
     int num;
     printf("Enter the number:");
     scanf("%d", &num);
     printf("Even = 1 | Odd = 0 : %d\n", EvenNumber(num));
 }
-int EvenNumber(int num)
-{
+int EvenNumber(int num) {
     if (num == 0) return 0;
-    return num % 2 == 0 ? 1 : EvenNumber(num / 10);
+    return num % 2 == 0 ? 1: EvenNumber(num / 10);
 }
-void Ex2()
-{
+void Ex2() {
     int num;
     printf("Enter the number:");
     scanf("%d", &num);
     printf("%d\n", Fibbonaci(num));
 }
-int Fibbonaci(int num)
-{
+int Fibbonaci(int num) {
     if (num == 0) return 0;
     if (num == 1) return 1;
     return Fibbonaci(num - 1) + Fibbonaci(num - 2);
 }
-void Ex3()
-{
+void Ex3() {
     int num;
     printf("Enter the number:");
     scanf("%d", &num);
     printf("Counter - %d\n", Counter(num));
     printf("Sum - %d\n", Sum(num));
-    printf("Sorted - %d\n");
+    printf("Sorted - %d\n", Sorted(num));
 }
-int Counter(int num)
-{
+int Counter(int num) {
     if (num == 0) return 0;
-    else return 1 + Counter(num / 10);
+    return 1 + Counter(num / 10);
 }
-int Sum(int num)
-{
+int Sum(int num) {
     if (num == 0) return 0;
-    else return num % 10 + Sum(num / 10);
+    return num % 10 + Sum(num / 10);
 }
-int Sorted(int num)
-{
-    if (num == 0) return 0;
+int Sorted(int num) {
+    if (num / 10 == 0) return 1;
+    return num % 10 < (num / 10) % 10 ? Sorted(num / 10): 0;
 }
-void Ex4()
-{
+void Ex4() {
+    int num;
+    printf("Enter the number:");
+    scanf("%d", &num);
+    printf("Counter - %d\n", CheckNumbers(num));
 }
-void Ex5()
-{
+int CheckNumbers(int num) {
+    int i;
+    printf("Enter a number:");
+    scanf("%d", &i);
+    if (i == -1) return 0;
+    return i < num ? 1 + CheckNumbers(num): CheckNumbers(num);
+}
+void Ex5() {
+    int num;
+    printf("Enter the number:");
+    scanf("%d", &num);
+    DigitsCounter(num);
+    printf("\n");
+    ReversedDigitsCounter(num);
+    printf("\n");
+    printf("%d", DigitsRange(num));
+}
+void DigitsCounter(int num) {
+    if (num == 0) return;
+    else printf("%d ", num);
+    return DigitsCounter(num - 1);
+}
+void ReversedDigitsCounter(int num) {
+    static int i = 1;
+    if (i <= num) printf("%d ", i++);
+    if (i > num) return;
+    return ReversedDigitsCounter(num);
+}
+int DigitsRange(int num) {
+    if (num == 1) return 1;
+    printf("%d", num);
+    return DigitsRange(num-1);
 }
