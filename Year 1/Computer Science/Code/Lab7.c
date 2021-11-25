23ef5cfc5ad895a6c43d0bf9c587c6c4384d413e
Art <artemgerasimov55@gmail.com>
2021-11-25 17:49:12 +0200

    Lab7

---
+++ Year 1/Computer Science/Code/Lab7.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define SIZE 5
+#include <stdio.h>
+// EX 1
+void Ex1();
+void InputArray(int*, int);
+void PrintArray(int*, int);
+//---------------------------
+// EX 2
+void Ex2();
+void InputArray(int*, int);
+void PrintArray(int*, int);
+//---------------------------
+void Ex1()
+{
+	int arr[SIZE];
+	InputArray(arr, SIZE);
+}
+void InputArray(int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("Enter %d's values:", i);
+		scanf("%d", &arr[i]);
+	}
+	PrintArray(arr, n);
+}
+void PrintArray(int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d", arr[i]);
+}
+void Ex2()
+{
+	int arr[SIZE];
+	InputArray(arr, SIZE);
+}
+int main()
+{
+	Ex1();
+}
