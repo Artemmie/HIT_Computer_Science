210d528efc2bbb7ea5d4cfe90b15c5a90b5e3674
Art <artemgerasimov55@gmail.com>
2021-11-29 14:43:16 +0200

    update Lab6-Addition-Ex.c

--- Year 1/Computer Science/Code/Lab6-Addition-Ex.c
+++ Year 1/Computer Science/Code/Lab6-Addition-Ex.c
@@ -14,29 +14,16 @@ void PrintArray(int*, int);
 void Ex1()
 {
 	int arr[SIZE];
-	InputArray(arr, SIZE);
-}
-void InputArray(int* arr, int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		printf("Enter %d's values:", i);
-		scanf("%d", &arr[i]);
-	}
-	PrintArray(arr, n);
-}
-void PrintArray(int* arr, int n)
-{
-	for (int i = 0; i < n; i++)
-		printf("%d", arr[i]);
+	InputArray(arr, SIZ
 }
 void Ex2()
 {
-	int arr[SIZE];
+	int num
 	InputArray(arr, SIZE);
 }
 int main()
 {
+    int num;
 	Ex1();
 	Ex2();
 }
