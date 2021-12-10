7bbb975dd3c5ca1b2e4d5df82876e5eb0fc2aa47
Art <artemgerasimov55@gmail.com>
2021-12-10 13:41:12 +0200

    update Lab7.c

--- Year 1/Computer Science/Code/Lab7.c
+++ Year 1/Computer Science/Code/Lab7.c
@@ -2,6 +2,7 @@
 #define SIZE 5
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 // EX 1
 void Ex1();
 void InputArray(int*, int);
@@ -9,7 +10,7 @@ void PrintArray(int*, int);
 //---------------------------
 // EX 2
 void Ex2();
-void arrCacl(int*, int, int*, int*, int*, double, int*);
+void arrCalc(int*, int, int*, int*, int*, double*, int*);
 //---------------------------
 
 int main()
@@ -25,20 +26,36 @@ void Ex1()
 }
 void InputArray(int* arr, int n)
 {
+ srand(time(NULL));
 	for (int i = 0; i < n; i++)
 	{
-		printf("Enter %d's values:", i);
-		arr[i] = rand();
+		arr[i] = rand() % 100;
 	}
 	PrintArray(arr, n);
 }
 void PrintArray(int* arr, int n)
 {
 	for (int i = 0; i < n; i++)
-		printf("%d\n", arr[i]);
+   printf("%d\n", arr[i]);
 }
 void Ex2()
 {
 	int arr[SIZE];
 	InputArray(arr, SIZE);
+ arrCalc(arr, SIZE,NULL,NULL,NULL,NULL,NULL)
+;
+}
+void arrCalc(int *arr, int n, int *max , int *min, int *sum, double *avg, int *isEven)
+{
+    min = INT_MAX;
+    max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (min > arr[i]) min = arr[i];
+        if (max < arr[i]) max = arr[i];
+        if (arr[i] % 2 == 0) isEven = 1;
+        sum += arr[i];  
+    }
+    avg = (*double)sum / n;
+    printf("Min: %d | Max: %d | Sum: %d | Avg: %d\n", min, max, sum, avg);
 }
