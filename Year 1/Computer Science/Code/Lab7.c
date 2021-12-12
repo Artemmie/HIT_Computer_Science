c3c11b5ff7ecf441d8b65a68a3f9f66eef438679
Art <artemgerasimov55@gmail.com>
2021-12-12 13:29:04 +0200

    update Lab7.c

--- Year 1/Computer Science/Code/Lab7.c
+++ Year 1/Computer Science/Code/Lab7.c
@@ -88,11 +88,15 @@ int SortedArr(int* arr, int n, int *flag) {
 }
 int SortedArrRecursion(int* arr, int num, int *flag) 
 {
-    if (num == 0) return 1;
+    if (num == 0) 
+    {
+        *flag = 1;
+    }
+    return 1;
     if arr[num - 1] < arr[num -2] {
         *flag = 0;
         return 0
     }
     if arr[num - 1] == arr[num -2] *flag = 0;
-    return SortedArrRecursion(arr, num - 1, flag) 
+    return SortedArrRecursion(arr, num - 1, flag)
 }
