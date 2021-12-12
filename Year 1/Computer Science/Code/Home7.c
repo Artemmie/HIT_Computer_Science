c0eedb95b31ace5ef1537f1d38069e8e8a61c0c1
Art <artemgerasimov55@gmail.com>
2021-12-12 17:12:42 +0200

    update Home7.c

--- Year 1/Computer Science/Code/Home7.c
+++ Year 1/Computer Science/Code/Home7.c
@@ -28,22 +28,7 @@ void SortPointers(int* num1ptr, int* num2ptr, int* num3ptr)
     int tmp = *num1ptr;
     if (*num1ptr <= *num2ptr)
     {
-        if (*num2ptr <= *num3ptr)
-        {
-            *num1ptr = *num3ptr;
-            *num3ptr = tmp;
-        }
-        else
-        {
-            *num1ptr = *num2ptr;
-            *num2ptr = tmp;
-        }
-    }
-    else if (*num1ptr <= *num3ptr)
-    {
-        *num1ptr = *num3ptr;
-        *num3ptr = *num2ptr;
-        *num2ptr = tmp;
+        if (*num2ptr <= *num3ptr);
     }
 }
 void Ex2()
