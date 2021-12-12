e8f376d19cfafd11ef7f9262df244f53dbf1be70
Art <artemgerasimov55@gmail.com>
2021-12-12 12:55:47 +0200

    update Lab7.c

--- Year 1/Computer Science/Code/Lab7.c
+++ Year 1/Computer Science/Code/Lab7.c
@@ -17,73 +17,67 @@ void Ex3();
 int SortedArr(int*, int, int*);
 int SortedArrRecursion(int*, int, int*);
 //---------------------------
-int main()
-{
-	//Ex1();
-	Ex2();
+int main() {
+    //Ex1();
+    Ex2();
     //Ex3();
 }
 
-void Ex1()
-{
-	int arr[SIZE];
-	InputArray(arr, SIZE);
+void Ex1() {
+    int arr[SIZE];
+    InputArray(arr, SIZE);
 }
-void InputArray(int* arr, int n)
-{
+void InputArray(int* arr, int n) {
     srand(time_t(NULL));
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;
-	}
-	PrintArray(arr, n);
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+    PrintArray(arr, n);
 }
-void PrintArray(int* arr, int n)
-{
+void PrintArray(int* arr, int n) {
     for (int i = 0; i < n; i++)
     printf("%d\n", arr[i]);
 }
-void Ex2()
-{
+void Ex2() {
     int isEven;
     int max = INT_MIN;
     int min = INT_MAX;
     int sum = 0;
     double avg = 0.0;
-	int arr[SIZE];
-	InputArray(arr, SIZE);
+    int arr[SIZE];
+    InputArray(arr, SIZE);
     arrCalc(arr, SIZE, &max, &min, &sum, &avg, &isEven);
     printf("Min: %d | Max: %d | Sum: %d | Avg: %0.2f | isEven: %d\n", min, max, sum, avg, isEven);
 }
-void arrCalc(int *arr, int n, int *max , int *min, int *sum, double *avg, int *isEven)
-{
-    for (int i = 0; i < n; i++)
-    {
+void arrCalc(int *arr, int n, int *max, int *min, int *sum, double *avg, int *isEven) {
+    for (int i = 0; i < n; i++) {
         if (*min > arr[i]) *min = arr[i];
         if (*max < arr[i]) *max = arr[i];
         if (arr[i] % 2 == 0) *isEven = 1;
-        *sum += arr[i];  
+        *sum += arr[i];
     }
     *avg = ((double)*sum / (double)n);
 
 }
-void Ex3()
-{
+void Ex3() {
     int flag = 2;
-    int arr[SIZE] = { 1, 3, 2, 5, 11 };
-	//InputArray(arr, SIZE);
+    int arr[SIZE] = {
+        1,
+        3,
+        2,
+        5,
+        11
+    };
+    //InputArray(arr, SIZE);
     int result = SortedArr(arr, SIZE, &flag);
     printf("Sorted: %d | Very Sorted: %d", result, flag);
     flag = -1;
     result = SortedArrRecursion(arr, SIZE, &flag);
     printf("Sorted: %d | Very Sorted: %d", result, flag);
 }
-int SortedArr(int* arr, int n, int *flag)
-{
-    for (int i = 0; i < n - 1 ; i++)
-    {
-        if (arr[i] > arr[i + 1]) 
-        {
+int SortedArr(int* arr, int n, int *flag) {
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
             *flag = 0;
             return 0;
         }
@@ -92,7 +86,6 @@ int SortedArr(int* arr, int n, int *flag)
     if (*flag != 0) *flag = 1;
     return 1;
 }
-int SortedArrRecursion(arr, SIZE, *flag)
-{
+int SortedArrRecursion(arr, SIZE, *flag) {
     
 }
