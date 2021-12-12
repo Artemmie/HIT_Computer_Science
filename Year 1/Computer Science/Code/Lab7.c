5b4b41405568a7c6f2ca171947fb87c0e7c3a9ed
Art <artemgerasimov55@gmail.com>
2021-12-12 16:39:08 +0200

    update Lab7.c

--- Year 1/Computer Science/Code/Lab7.c
+++ Year 1/Computer Science/Code/Lab7.c
@@ -150,9 +150,9 @@ void ReverseArr(int* arr, int n) {
 }
 void ReverseArrRecursion(int* arr, int n)
 {
-    if (n == 0) return;
+    if (n < 1) return;
     //int tmp = arr[0];
     //arr[0] = arr[n-1];
     //arr[n-1] = tmp;
-    ReverseArrRecursion(arr, n - 1);
+    ReverseArrRecursion(arr, n - 2);
 }
