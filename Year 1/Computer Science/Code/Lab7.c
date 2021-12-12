9b1c2384ea4439d070cd95dd4ca54b2d53c2f014
Art <artemgerasimov55@gmail.com>
2021-12-12 16:14:44 +0200

    update Lab7.c

--- Year 1/Computer Science/Code/Lab7.c
+++ Year 1/Computer Science/Code/Lab7.c
@@ -133,7 +133,6 @@ void Ex5() {
         5,
         6
     };
-    PrintArray(arr, SIZE);
     ReverseArr(arr, SIZE);
     printf("After:\n");
     PrintArray(arr, SIZE);
@@ -141,10 +140,10 @@ void Ex5() {
 void ReverseArr(int* arr, int n) {
     int tmp;
     for (int i = 0; i < n/2; i++) {
-        
+        printf("I: %d | n - i: %d | tmp: %d\n", arr[i], arr[n-i], tmp);
         tmp = arr[i];
         arr[i] = arr[n-i];
         arr[n-i] = tmp;
-        printf("I: %d | n - i: %d | tmp: %d\n", arr[i], arr[n-i], tmp);
+        
     }
 }
