ad0662b1d7590ae9fdfc61ff3ff8cf40f38c0770
Art <artemgerasimov55@gmail.com>
2021-12-13 13:22:21 +0200

    update Home7.c

--- Year 1/Computer Science/Code/Home7.c
+++ Year 1/Computer Science/Code/Home7.c
@@ -23,7 +23,7 @@ int BiggestCombination(int*, int);
 //---------------------------
 // EX 5
 void Ex5();
-int BiggestCombination(int*, int);
+int BiggestCombination(int*, int, int);
 //---------------------------
 void main()
 {
