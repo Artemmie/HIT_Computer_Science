2d6a62491a836142ffd3d4e06149f1e509d212f5
Art <artemgerasimov55@gmail.com>
2021-12-13 10:46:04 +0200

    update Home7.c

--- Year 1/Computer Science/Code/Home7.c
+++ Year 1/Computer Science/Code/Home7.c
@@ -46,7 +46,8 @@ void SortPointers(int* num1ptr, int* num2ptr, int* num3ptr)
 }
 void Ex2()
 {
-    int num = 928743, digit = 4, counter = 0;
+    int num = 928743, digit = 4, 
+    int counter = 0;
     double result = AverageAboveDig(num, digit, &counter);
     printf("Average: %.2f | counter: %d", result, counter);
 }
