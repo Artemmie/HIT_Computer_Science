f8ac7442cdc15a16a0c5a49ba34b2bcc987a008d
Art <artemgerasimov55@gmail.com>
2021-12-13 13:06:13 +0200

    update Home7.c

--- Year 1/Computer Science/Code/Home7.c
+++ Year 1/Computer Science/Code/Home7.c
@@ -90,7 +90,7 @@ int Infi(int x, int *c, int *n)
 int IntPow(int x, int y)
 {
     int index = x;
-    printf("x: %d | y: %d", x, y);
+    printf("x: %d | y: %d\n", x, y);
     for (int i = 0; i < y; i++) x *= index;
     return x;
 }
