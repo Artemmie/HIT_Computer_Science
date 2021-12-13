c008eacdf9fe01b5fb0148f2f2eb2a85e224ff83
Art <artemgerasimov55@gmail.com>
2021-12-13 11:04:47 +0200

    update Home7.c

--- Year 1/Computer Science/Code/Home7.c
+++ Year 1/Computer Science/Code/Home7.c
@@ -59,7 +59,7 @@ double AverageAboveDig(int num, int digit, int *counter)
         if (num % 10 < digit)
         {
             *counter += 1;
-            result += (double)(num % 10)
+            result += (double)(num % 10);
         }
         num /= 10;
     }
