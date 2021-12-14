a5c3c4b5ff2765e496686abd6c3fc9b81d619961
Art <artemgerasimov55@gmail.com>
2021-12-14 08:36:39 +0200

    update Home7.c

--- Year 1/Computer Science/Code/Home7.c
+++ Year 1/Computer Science/Code/Home7.c
@@ -310,7 +310,7 @@ int Super(int *arr, int n)
 }
 int SuperRec(int* arr, int n)
 {
-    if (n == 2) return 1;
+    if (n == 1) return 1;
     SuperRec(arr, n - 1);
     if(arr[0] > arr[n-1]) return 0;
     arr[0] += arr[n-1];
