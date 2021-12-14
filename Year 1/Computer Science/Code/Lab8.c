606c281186146a3eef191f3498cb63a075d0a49d
Art <artemgerasimov55@gmail.com>
2021-12-14 15:16:31 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -111,7 +111,7 @@ int Symmetrical(int *arr, int n, int k)
 }
 void Ex5()
 {
-    int arr[6] = {5,1, 6, 6, 9, 9};
+    int arr[8] = {5,5, 1, 4, 4, 4,7 ,7};
     int a = 1, b = 9;
     int result = RangeArr(arr, 6, a, b);
     printf("Result: %d", result);
