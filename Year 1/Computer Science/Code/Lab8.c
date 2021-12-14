94f6c988d439f2605a3095effa300d999dd38e9f
Art <artemgerasimov55@gmail.com>
2021-12-14 14:15:39 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -102,7 +102,7 @@ int Symmetrical(int *arr, int n, int k)
         resultarr[arr[i]]++;
     for (int i = 0; i <= k; i++)
     {
-        printf("%d", resultarr[i]);
+        printf("{%d}", resultarr[i]);
         if (resultarr[i] != i + 1) return 0;
     }
 
