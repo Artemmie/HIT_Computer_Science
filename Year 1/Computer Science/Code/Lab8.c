0552121292040c1e6cf252765edeb26384c1e681
Art <artemgerasimov55@gmail.com>
2021-12-14 13:16:48 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -62,5 +62,6 @@ void Ex3()
 {
     int arr[SIZE] = {1,6, 3, 3, 6};
     int count = 0;
+    int result = Popularity(arr, SIZE, &count)
     printf("Result: %d | Count: %d", Popularity(arr, SIZE, &count), count);
 }
