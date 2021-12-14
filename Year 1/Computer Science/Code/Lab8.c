8082d504fb21b4b6d0cb962589122fa4cca32ce4
Art <artemgerasimov55@gmail.com>
2021-12-14 14:37:18 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -119,4 +119,8 @@ int Range(int *arr, int n, int a, int b)
 {
     int *resultarr = calloc(b - a + 1, sizeof(int));
     for(int i = 0; i < n; i++) resultarr[arr[i]]++;
+    for (a; a < b; a++)
+    {
+        
+    }
 }
