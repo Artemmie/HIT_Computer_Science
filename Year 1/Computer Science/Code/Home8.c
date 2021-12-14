f15a65b84b33326ee95aa9979fbd7f50c3b31c70
Art <artemgerasimov55@gmail.com>
2021-12-14 16:09:46 +0200

    update Home8.c

--- Year 1/Computer Science/Code/Home8.c
+++ Year 1/Computer Science/Code/Home8.c
@@ -30,8 +30,5 @@ int * Lucas(int size);
 }
 void PrintArray(int *arr, int n)
 {
-    for (int i = 0; i <= n; i++)
-    {
-        
-    }
+    for (int i = 0; i <= n; i++) printf("{%d}", arr[i]);
 }
