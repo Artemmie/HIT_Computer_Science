b68d4343a050f21c620158bd683ba8f4c2107737
Art <artemgerasimov55@gmail.com>
2021-12-14 13:24:36 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -70,8 +70,6 @@ int result(int *arr, int n, int *count)
     int *resultarr = calloc(size, sizeof(int));
     int result;
     for (int i = 0; i < n; i++)
-    {
         resultarr[arr[i]]++;
-    }
     
 }
