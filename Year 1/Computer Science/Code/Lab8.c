b916fcf2bb73383e2bcdbdcfc140aa2fedddf2b0
Art <artemgerasimov55@gmail.com>
2021-12-14 09:08:50 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -20,7 +20,7 @@ int * BuildArray(int size)
 {
     int *arr = (int*)malloc(size * (sizeof(int)));
     for (int i = 0; i < size; i++)
-        arr[i] = i + 1;
+        arr[i] = i + 1; //SCANF
     return arr;
 }
 void PrintArray(int* arr, int size)
