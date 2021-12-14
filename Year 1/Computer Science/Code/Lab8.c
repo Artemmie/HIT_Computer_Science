49858f3c4f37adcf013b1c32d8a02c8a5d624059
Art <artemgerasimov55@gmail.com>
2021-12-14 12:35:30 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -31,7 +31,7 @@ int * BuildArray(int size)
 }
 void PrintArray(int* arr, int size)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i <= size; i++)
         printf("{%d}", arr[i]);
 }
 void Ex2()
