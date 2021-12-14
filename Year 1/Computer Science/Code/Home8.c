a79018d2c3d5f8c66a138e9d578cd3cd1c38badb
Art <artemgerasimov55@gmail.com>
2021-12-14 16:55:50 +0200

    update Home8.c

--- Year 1/Computer Science/Code/Home8.c
+++ Year 1/Computer Science/Code/Home8.c
@@ -61,7 +61,11 @@ int * ArrayChange(int *arr, int n, int isEven, int *size)
             newArr[*size] = arr[i];
             *size = *size + 1;
         }
-        else if (isEven == 0 && arr[i] % 2 != 0) newArr[*size] = arr[i];
+        else if (isEven == 0 && arr[i] % 2 != 0) 
+        {
+            
+        }
+        newArr[*size] = arr[i];
         
     }
     return newArr;
