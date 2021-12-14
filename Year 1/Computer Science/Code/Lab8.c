5d64958a3fc97387320b49f1263432280ec77a19
Art <artemgerasimov55@gmail.com>
2021-12-14 14:39:30 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -118,7 +118,7 @@ void Ex5()
 }
 int Range(int *arr, int n, int a, int b)
 {
-    int max = INT_MIN, min = b + 1;
+    int max = INT_MIN, min = INT_MAX;
     int *resultarr = calloc(b - a + 1, sizeof(int));
     for(int i = 0; i < n; i++) resultarr[arr[i]]++;
     for (int i = a; i <= b; i++)
