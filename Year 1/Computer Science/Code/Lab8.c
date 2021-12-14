892b3a9a9b65d1d65ed4e2c6f7ffb530f09c034d
Art <artemgerasimov55@gmail.com>
2021-12-14 14:33:57 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -117,6 +117,6 @@ void Ex5()
 {
     int arr[8] = {5,5, 1, 4, 4, 4, 7, 7};
     int a = 1, b = 8;
-    int result = Symmetrical(arr, SIZE, k);
+    int result = Range(arr, SIZE, k);
     printf("Result: %d", result);
 }
