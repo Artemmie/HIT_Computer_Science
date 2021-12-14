3fde5cd10e5b3b76a75f30b3451e1827de25e874
Art <artemgerasimov55@gmail.com>
2021-12-14 09:05:26 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -14,7 +14,7 @@ void Ex1()
     int size = 10;
     int *arr = BuildArray(size);
 }
-int* BuildArray(int size)
+int * BuildArray(int size)
 {
     int *arr = (int*)malloc(size * (sizeof(int)));
     for (int i = 0; i < size; i++){
