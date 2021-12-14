65c10f2235c7ecdfe42549a7d422d93ff8665d3c
Art <artemgerasimov55@gmail.com>
2021-12-14 16:52:23 +0200

    update Home8.c

--- Year 1/Computer Science/Code/Home8.c
+++ Year 1/Computer Science/Code/Home8.c
@@ -55,6 +55,7 @@ int * ArrayChange(int *arr, int n, int isEven, int *size)
             newArr = realloc(newArr, (*size +1) * sizeof(int));
         if(isEven == 1 && arr[i] % 2 == 0) newArr[*size] = arr[i];
         else if (isEven == 0 && arr[i] % 2 != 0) newArr[*size] = arr[i];
+        printf("{%d}", newArr[*size]);
         *size = *size + 1;
     }
     return newArr;
