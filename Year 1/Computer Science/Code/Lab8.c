2750d2d3c5c64c6cdc3784c3b52b6635c97de57a
Art <artemgerasimov55@gmail.com>
2021-12-14 13:37:17 +0200

    update Lab8.c

--- Year 1/Computer Science/Code/Lab8.c
+++ Year 1/Computer Science/Code/Lab8.c
@@ -77,7 +77,7 @@ int Popularity(int *arr, int n, int *count)
         printf("i: %d, result: %d\n", resultarr[i], result);
         if (resultarr[i] > result)
         {
-            result = resultarri;
+            result = resultarr[i];
             *count = resultarr[i];
         }
     }
