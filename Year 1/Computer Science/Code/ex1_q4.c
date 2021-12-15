bd308891fbd32514fe43e767411867c42d750c1d
Art <artemgerasimov55@gmail.com>
2021-12-15 14:53:22 +0200

    update ex1_q4.c

--- Year 1/Computer Science/Code/ex1_q4.c
+++ Year 1/Computer Science/Code/ex1_q4.c
@@ -50,7 +50,7 @@ int sum_proper_divisors(int num)
 {
 	// Your code:
 	int sum = 0;
-    for (int i = 1; i < num / 2; i++) 
+    for (int i = 1; i < 9; i++) 
         if (num % i == 0) sum += i;
 }
 
