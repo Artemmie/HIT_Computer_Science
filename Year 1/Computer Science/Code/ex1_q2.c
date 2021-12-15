53541c72842fb9970c216486579dba34a1b03e5d
Art <artemgerasimov55@gmail.com>
2021-12-15 14:37:30 +0200

    update ex1_q2.c

--- Year 1/Computer Science/Code/ex1_q2.c
+++ Year 1/Computer Science/Code/ex1_q2.c
@@ -49,7 +49,7 @@ unsigned long student_id()
 int longest_seq(int n)
 {
 // Your code:
-    int mm, index = 0, seq = 0, max_seq = 0;
+    int mm, index = 0, seq = 0, max_seq = 0, flag = 0;
     for (int i = 1; i <= n; i++)
     {
         printf("Enter value for day number %d (in millimetres): ", i);
@@ -60,6 +60,7 @@ int longest_seq(int n)
             continue;
         }
         seq++;
+        
     }
     return index;
 }
