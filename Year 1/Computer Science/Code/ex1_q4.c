95015d37078b6ea544db8e0d3c8f349a241b1490
Art <artemgerasimov55@gmail.com>
2021-12-15 14:47:37 +0200

    update ex1_q4.c

---
+++ Year 1/Computer Science/Code/ex1_q4.c
@@ -0,0 +1,68 @@
+// Course: introduction to computer science
+// exercise 1, question 4
+// file name: ex1_q4.c
+#define _CRT_SECURE_NO_WARNINGS
+// --------------------------- //
+// include package section:
+#include <stdio.h>
+// --------------------------- //
+
+
+// --------------------------- //
+// function declaration section:
+unsigned long student_id();
+int sum_proper_divisors(int);
+void amicable_numbers(int);
+void print_amicable_pair(int, int);
+// --------------------------- //
+
+
+// --------------------------- //
+// DO NOT CHANGE the following function:
+void main()
+{
+	unsigned long id_num;
+	int num;
+	// read input:
+	printf("Enter a number and the program will print all the amicable numbers in this field: ");
+	scanf("%d", &num);
+	// call functions:
+	id_num = student_id();
+	amicable_numbers(num);
+	// write output:
+	printf("[id: %lu] finished main\n", id_num);
+}
+// --------------------------- //
+
+
+// --------------------------- //
+// function implementation section:
+unsigned long student_id()
+{
+	// return your id muber,
+	// for example if your id is 595207432
+	// return 595207432
+	// your code:
+
+}
+
+int sum_proper_divisors(int num)
+{
+	// Your code:
+
+}
+
+void amicable_numbers(int num)
+{
+	// Your code:
+
+}
+
+
+// DO NOT CHANGE the following function:
+void print_amicable_pair(int num1, int num2)
+{
+	// write output:
+	printf("(%4d ,  %4d )\n", num1, num2);
+}
+// --------------------------- //
