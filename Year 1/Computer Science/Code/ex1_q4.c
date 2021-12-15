0aeb9bb2e95b9a18f3f5cca7146782726b80cf15
Art <artemgerasimov55@gmail.com>
2021-12-15 14:50:37 +0200

    update ex1_q4.c

--- Year 1/Computer Science/Code/ex1_q4.c
+++ Year 1/Computer Science/Code/ex1_q4.c
@@ -50,10 +50,7 @@ int sum_proper_divisors(int num)
 {
 	// Your code:
 	int sum = 0;
-    for (int i = 1; i < num / 2; i++)
-    {
-        if (num % i == 0) sum += i;
-    }
+    for (int i = 1; i < num / 2; i++) if (num % i == 0) sum += i;
 }
 
 void amicable_numbers(int num)
