2c569d0c8b4628da6e2560ca3a3c6c4489ab39e5
Art <artemgerasimov55@gmail.com>
2021-12-15 09:21:19 +0200

    update Home8.c

--- Year 1/Computer Science/Code/Home8.c
+++ Year 1/Computer Science/Code/Home8.c
@@ -76,7 +76,7 @@ void Ex3()
     int arrB[3] = {3,3,3};
     int *result = Vectors(arrA, arrB, 3);
     PrintArray(result, 3);
-    printf("\n%d", result[3]);
+    printf("\n%d", result[4]);
 }
 int * Vectors(int *arrA, int *arrB, int size)
 {
