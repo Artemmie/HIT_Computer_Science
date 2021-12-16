4097e3432b775d8ef0c9d031ea490815a5c72369
Art <artemgerasimov55@gmail.com>
2021-12-16 17:31:43 +0200

    Update Home8.c

--- Year 1/Computer Science/Code/Home8.c
+++ Year 1/Computer Science/Code/Home8.c
@@ -6,15 +6,15 @@ void PrintArray(int*, int);
 //---------------------------
 // EX 1
 void Ex1();
-int * Lucas(int);
+int* Lucas(int);
 //---------------------------
 // EX 2
 void Ex2();
-int * ArrayChange(int*, int, int, int*);
+int* ArrayChange(int*, int, int, int*);
 //---------------------------
 // EX 3
 void Ex3();
-int * Vectors(int*, int*, int);
+int* Vectors(int*, int*, int);
 //---------------------------
 // EX 4
 void Ex4();
@@ -22,20 +22,20 @@ void ArrayMultip(int*, int);
 //---------------------------
 int main()
 {
-     //Ex1();
-    //Ex2();
-    //Ex3();
+    //Ex1();
+   //Ex2();
+   //Ex3();
     Ex4();
     return 0;
 }
 void Ex1()
 {
     int size = 9;
-    int *arr = Lucas(size);
+    int* arr = Lucas(size);
 }
-int * Lucas(int size)
+int* Lucas(int size)
 {
-    int *arr = (int*)malloc((size + 1) * sizeof(int));
+    int* arr = (int*)malloc((size + 1) * sizeof(int));
     for (int i = 0; i <= size; i++)
     {
         if (i == 0) arr[i] = 2;
@@ -44,30 +44,30 @@ int * Lucas(int size)
     }
     return arr;
 }
-void PrintArray(int *arr, int n)
+void PrintArray(int* arr, int n)
 {
     for (int i = 0; i < n; i++) printf("{%d}", arr[i]);
 }
 void Ex2()
 {
-    int arr[5] = {1,8,3,6,11};
+    int arr[5] = { 1,8,3,6,11 };
     int isEven = 1;
     int size = 0;
-    int *newArr = ArrayChange(arr, 5, isEven, &size);
+    int* newArr = ArrayChange(arr, 5, isEven, &size);
     PrintArray(newArr, size);
     printf("\nSize - %d", size);
 }
-int * ArrayChange(int *arr, int n, int isEven, int *size)
+int* ArrayChange(int* arr, int n, int isEven, int* size)
 {
-    int *newArr = (int*)malloc((*size) * sizeof(int));
+    int* newArr = (int*)malloc((*size) * sizeof(int));
     for (int i = 0; i < n; i++)
     {
-        if(isEven == 1 && arr[i] % 2 == 0) 
+        if (isEven == 1 && arr[i] % 2 == 0)
         {
             newArr[*size] = arr[i];
             *size = *size + 1;
         }
-        else if (isEven != 1 && arr[i] % 2 != 0) 
+        else if (isEven != 1 && arr[i] % 2 != 0)
         {
             newArr[*size] = arr[i];
             *size = *size + 1;
@@ -77,14 +77,14 @@ int * ArrayChange(int *arr, int n, int isEven, int *size)
 }
 void Ex3()
 {
-    int arrA[3] = {1,2,1};
-    int arrB[3] = {3,3,3};
-    int *result = Vectors(arrA, arrB, 3);
+    int arrA[3] = { 1,2,1 };
+    int arrB[3] = { 3,3,3 };
+    int* result = Vectors(arrA, arrB, 3);
     PrintArray(result, 3);
 }
-int * Vectors(int *arrA, int *arrB, int size)
+int* Vectors(int* arrA, int* arrB, int size)
 {
-    int *result = (int*)malloc(size * sizeof(int));
+    int* result = (int*)malloc(size * sizeof(int));
     result[0] = arrA[1] * arrB[2] - arrA[2] * arrB[1];
     result[1] = arrA[2] * arrB[0] - arrA[0] * arrB[2];
     result[2] = arrA[0] * arrB[1] - arrA[1] * arrB[0];
@@ -92,15 +92,26 @@ int * Vectors(int *arrA, int *arrB, int size)
 }
 void Ex4()
 {
-    int arr[5] = {1,2,2,4,1};
+    int arr[5] = { 1,2,2,4,1 };
     ArrayMultip(arr, 5);
     PrintArray(arr, 5);
 }
-void ArrayMultip(int *arr, int size)
+void ArrayMultip(int* arr, int size)
 {
-    int *tmpArr = (int*)malloc(size * sizeof(int))
+    int* tmpArr = (int*)malloc(size * sizeof(int));
     for (int i = 0; i < size; i++)
     {
-        
+        tmpArr[i] = arr[i];
+        for (int j = 0; j < size; i++)
+        {
+            if (i == j) continue;
+            tmpArr[i] *= j;
+        }
     }
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = tmpArr[i];
+    }
+    free(*tmpArr);
+    *tmpArr = NULL;
 }
