1b088a6fc42da1092447f1661128a101fd677f4a
Art <artemgerasimov55@gmail.com>
2021-12-19 13:16:51 +0200

    update ex1_q3.c

--- Year 1/Computer Science/Code/ex1_q3.c
+++ Year 1/Computer Science/Code/ex1_q3.c
@@ -59,7 +59,7 @@ double sum_seq(int n, double x)
     for (int i = 1; i <= n; i++)
     {
         divisor * = i;
-        divider *= powX 
+        divider *= powX; 
         if (i % 2 == 0) result += divider / divisor;
         else result -= divider / divisor;
     }
