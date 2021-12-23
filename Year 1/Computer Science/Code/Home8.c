8cc99ee5bac40e194e5f50bac0ae2c781d83c55e
Art <artemgerasimov55@gmail.com>
2021-12-23 10:38:11 +0200

    update Home8.c

--- Year 1/Computer Science/Code/Home8.c
+++ Year 1/Computer Science/Code/Home8.c
@@ -222,5 +222,9 @@ void RotateArray(int* arr, int n, int count)
     for (int i = 0; i < count; i++)
     {
         int lastVar = arr[n-1];
+        for (int i = 0; i < n; i++)
+        {
+            
+        }
     }
 }
