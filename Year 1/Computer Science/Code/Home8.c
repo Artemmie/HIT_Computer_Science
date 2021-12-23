a2f88fc16d1a92da87b7e8413f86826d41b6b5d1
Art <artemgerasimov55@gmail.com>
2021-12-23 09:54:28 +0200

    update Home8.c

--- Year 1/Computer Science/Code/Home8.c
+++ Year 1/Computer Science/Code/Home8.c
@@ -174,7 +174,7 @@ int BiggestSums(int* arr, int n)
     int maxVal = 0;
     for (int i = 0; i < n; i++)
         newArr[arr[i]]++;
-    for (int i = 0; < n+1; i++)
+    for (int i = 0; <= n; i++)
     {
         
     }
